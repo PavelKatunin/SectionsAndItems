b94e2e761fd6186130f942c435087429654c32c3
Pavel Katunin <pavelkatunin@pavels-mbp.home>
2024-07-22 16:18:27 +0100

    sections implemented

--- SectionsAndItems/SectionsAndItems/ItemView.swift
+++ SectionsAndItems/SectionsAndItems/ItemView.swift
@@ -3,17 +3,21 @@ import SwiftUI
 
 struct ItemView: View {
     
-    @State var text: String
+    @State private var text: String
+    
+    init(text: String) {
+        self.text = text
+    }
     
     var body: some View {
         ZStack {
             Text("\(text)").font(.title)
         }
         .onAppear() {
-            print("SiwftUI event: Item \(text) appeared")
+            // print("SiwftUI event: Item \(text) appeared")
         }
         .onDisappear() {
-            print("SiwftUI event: Item \(text) disappeared")
+            // print("SiwftUI event: Item \(text) disappeared")
         }
     }
     
